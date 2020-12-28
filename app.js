bdbcf765a7ebae56c6e618aedbfb35d7ee6a2554
alokeveermondal <mondalalokeveer2001@gmail.com>
2020-12-28 18:41:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookie = require('cookie-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+app.set('view engine', 'ejs');
+
+const dbURI='mongodb+srv://alokeveer2001:alokeveer2001@cluster0.vhxns.mongodb.net/tutorials?retryWrites=true&w=majority';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+
+app.get('/', (req, res) => res.render('home', {title: 'home'}));
+
